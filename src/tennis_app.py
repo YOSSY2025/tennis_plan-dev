7c4cdb6e2419422fc2d9f3ad03ba526685e78c2e
YOSSY2025 <t.yoshitani1979@gmail.com>
2025-12-30 00:12:06 +0900

    Update tennis_app.py

--- src/tennis_app.py
+++ src/tennis_app.py
@@ -367,7 +367,6 @@ with tab_list:
             df_display,
             use_container_width=True,
             hide_index=True,
-            on_select="rerun",
             selection_mode="single-row",
             key=table_key,
             column_config={
